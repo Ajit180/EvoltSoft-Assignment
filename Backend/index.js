4332c9302b75b05577c93f15248d58dc50a7088e
Ajit <yajit1908@gmail.com>
2025-06-03 13:24:10 +0530

    Added Cors

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,30 @@ import express from 'express'
 import { PORT } from './src/config/serverconfig.js';
 import connnectdb from './src/config/dbConfig.js';
 import apiRoutes from '../Backend/src/routers/apiRoutes.js'
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    'http://localhost:3000',
+  ];
+
+
+  app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 
 app.get('/pong',(req,res)=>{
