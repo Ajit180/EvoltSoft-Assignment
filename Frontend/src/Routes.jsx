36ebd62558429814c6fa694c322badc774635185
Ajit <yajit1908@gmail.com>
2025-06-03 14:58:06 +0530

    fix:import issue

--- Frontend/src/Routes.jsx
+++ Frontend/src/Routes.jsx
@@ -1,10 +1,10 @@
-import Auth from '@/pages/auth/Auth'
-import { SigninContainer } from '@/pages/auth/Signin'
-import { SignupContainer } from '@/pages/auth/Signup'
+import Auth from './pages/Auth/Auth'
+import { SigninContainer } from '@/pages/Auth/Signin'
+import { SignupContainer } from '@/pages/Auth/Signup'
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import ProtectedRoute from './components/Component/ProtectedRoute'
-import Station from './components/Component/Station'
+// import ProtectedRoute from './components/Component/ProtectedRoute'
+// import Station from './components/Component/Station'
 
 
 const AppRoutes = () => {
@@ -12,7 +12,7 @@ const AppRoutes = () => {
     <Routes>
         <Route path='auth/signup' element={<Auth><SignupContainer/></Auth>}/>
         <Route path='auth/signin' element={<Auth><SigninContainer/></Auth>}/>
-        <Route path='auth/station' element ={<ProtectedRoute><Station/></ProtectedRoute>}/>
+        {/* <Route path='auth/station' element ={<ProtectedRoute><Station/></ProtectedRoute>}/> */}
        
     </Routes>
   )
